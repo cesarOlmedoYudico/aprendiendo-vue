a92e08a863222e53298084ba2d68e78419a322e5
cesarOlmedoYudico <peluditoxx@gmail.com>
2022-10-06 18:13:16 -0500

    agregar objetoFruta y recorrer sus  propiedades

--- primerProyectoVue/src/App.vue
+++ primerProyectoVue/src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
 const name= `Vue dinamico`;
 const styleColor=`color: blue`;
+const objetoFruta ={
+            name: "Manzana",
+            price: "$1.00",
+            description: "Una manzana",
+      }
 </script>
 
 
 <template>
   <h1>Hola {{name.toUpperCase()}}</h1><br>
   <h2 :style="styleColor">soy azul</h2>
+
+<li v-for="value in objetoFruta" :key="value">
+  {{value}}
+</li>
+
 </template>
 
 
