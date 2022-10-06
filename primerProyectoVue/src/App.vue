5a6beb974d9cc3928a624f48a620f2cb84e5645d
cesarOlmedoYudico <peluditoxx@gmail.com>
2022-10-06 15:06:57 -0500

    proyecto iniciado

---
+++ primerProyectoVue/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+const name= `Vue dinamico`;
+const styleColor=`color: blue`;
+</script>
+
+
+<template>
+  <h1>Hola {{name.toUpperCase()}}</h1><br>
+  <h2 :style="styleColor">soy azul</h2>
+</template>
+
+
+
+
+<style>
+  h1{
+    color:red;
+  }
+</style>
